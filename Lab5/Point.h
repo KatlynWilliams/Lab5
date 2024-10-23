b86158e5de81a3d2113f439518796741c9993f78
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-23 00:01:50 -0500

    Update Point.h

--- Lab5/Point.h
+++ Lab5/Point.h
@@ -1,32 +1,29 @@
 #pragma once
 #include <iostream>
-#include <ostream>
 
-class Point
-{
+class Point {
 private:
-	int x;
-	int y;
+    int x;
+    int y;
 public:
-	Point(int x, int y);
+    Point(int x, int y);
 
-	//Equality Operators
-	bool operator==(const Point& other) const;
-	bool operator!=(const Point& other) const;
+    // Equality Operators
+    bool operator==(const Point& other) const;
+    bool operator!=(const Point& other) const;
 
-	//Comparison Operators
-	bool operator<(const Point& other) const;
-	bool operator>(const Point& other) const;
-	bool operator<=(const Point& other) const;
-	bool operator>=(const Point& other) const;
+    // Comparison Operators
+    bool operator<(const Point& other) const;
+    bool operator>(const Point& other) const;
+    bool operator<=(const Point& other) const;
+    bool operator>=(const Point& other) const;
 
-	//Insertion Operator
-	friend std::ostream& operator<<(std::ostream& stream, const Point& point);
+    // Insertion Operator
+    friend std::ostream& operator<<(std::ostream& stream, const Point& point);
 
-	//Arithmetic Operator
-	Point& operator+(int value);
+    // Arithmetic Operator
+    Point operator+(int value) const;
 
-	//Compound Assignment Operator
-	Point& operator+=(int value);
+    // Compound Assignment Operator
+    Point& operator+=(int value);
 };
-
